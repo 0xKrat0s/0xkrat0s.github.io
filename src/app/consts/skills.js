3d028e954b760d2020500cb0953cdc2fa4b2e195
0xKrat0s <amalpkkratos@gmail.com>
2025-05-05 22:53:32 +0530

    Deleted whole contents

--- src/app/consts/skills.js
+++
@@ -1,7 +0,0 @@
-export default {
-    language: ["python", "bash"],
-    security: ["web application pentesting", "API pentesting", "mobile app pentesting", "cloud security", "thick client pentesting", "network pentesting"],
-    tools: ["burp suite", "nessus", "wireshark", "postman", "semgrep"],
-    frameworks: ["owasp", "nist", "mitre att&ck"],
-    certifications: ["ceh"]
-}
